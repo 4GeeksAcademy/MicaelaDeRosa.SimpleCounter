6ae36cc097a08d5ec3e70870ac536f1c72b33b5f
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-10-23 22:44:11 +0000

    SimpleCounter

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+function SimpleCounter(props) {
+    return (<div className="bigCounter">
+        <div className="calendar">
+            <i class="bi bi-alarm"></i>
+        </div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
+    </div>);
+}
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+};
+
+export default SimpleCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,8 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
+import ReactDOM from 'react-dom/client';
+import SimpleCounter from './component/SimpleCounter';
 import '../styles/index.css';
-
-function SimpleCounter(props) {
-    return (<div className="bigCounter">
-        <div className="calendar">
-            <i class="bi bi-alarm"></i>
-        </div>
-        <div className="four">{props.digitFour % 10}</div>
-        <div className="three">{props.digitThree % 10}</div>
-        <div className="two">{props.digitTwo % 10}</div>
-        <div className="one">{props.digitOne % 10}</div>
-    </div>);
-}
-SimpleCounter.propTypes = {
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne: PropTypes.number,
-};
 
 let counter = 0;
 setInterval(function () {
@@ -32,10 +13,7 @@ setInterval(function () {
     console.log(four, three, two, one);
     //render your react application
     counter++;
-    ReactDOM.render(
-        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
-        document.querySelector('#app')
-    );
+    ReactDOM.createRoot(document.getElementById('app')).render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />)
 }, 1000);
 
 
